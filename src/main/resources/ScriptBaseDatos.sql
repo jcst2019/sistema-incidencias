58b9bd61e191809f259b7050adce1225892718a1
jcst2019 <juansolar_4@hotmail.com>
2023-11-06 21:13:10 -0500

    ajustes finales

--- src/main/resources/ScriptBaseDatos.sql
+++ src/main/resources/ScriptBaseDatos.sql
@@ -177,6 +177,25 @@ VALUES
  (2,3,2,1,'Asunto 5','Descripcion 1',NOW(),11,null, 'A');
 
 
+INSERT INTO incidencias (idServicio, idSubcategoria, idImpacto, idUrgencia, asunto, descripcion, fechaRegistro, idUsuarioRegistra, estado)
+VALUES 
+(1, 1, 1, 1, 'Incidencia 1', 'Descripción de la incidencia 1', NOW(), 1, 'A'),
+(2, 2, 2, 2, 'Incidencia 2', 'Descripción de la incidencia 2', NOW(), 2, 'A'),
+(3, 3, 3, 3, 'Incidencia 3', 'Descripción de la incidencia 3', NOW(), 3, 'A'),
+(1, 4, 1, 4, 'Incidencia 4', 'Descripción de la incidencia 4', NOW(), 4, 'A'),
+(2, 5, 2, 1, 'Incidencia 5', 'Descripción de la incidencia 5', NOW(), 5, 'A'),
+(3, 6, 3, 2, 'Incidencia 6', 'Descripción de la incidencia 6', NOW(), 6, 'A'),
+(1, 7, 1, 3, 'Incidencia 7', 'Descripción de la incidencia 7', NOW(), 7, 'A'),
+(2, 8, 2, 4, 'Incidencia 8', 'Descripción de la incidencia 8', NOW(), 8, 'A'),
+(3, 9, 3, 1, 'Incidencia 9', 'Descripción de la incidencia 9', NOW(), 9, 'A'),
+(1, 10, 1, 2, 'Incidencia 10', 'Descripción de la incidencia 10', NOW(), 10, 'A'),
+(2, 11, 2, 3, 'Incidencia 11', 'Descripción de la incidencia 11', NOW(), 11, 'A'),
+(3, 12, 3, 4, 'Incidencia 12', 'Descripción de la incidencia 12', NOW(), 1, 'A'),
+(1, 1, 1, 1, 'Incidencia 13', 'Descripción de la incidencia 13', NOW(), 2, 'A'),
+(2, 2, 2, 2, 'Incidencia 14', 'Descripción de la incidencia 14', NOW(), 3, 'A'),
+(3, 3, 3, 3, 'Incidencia 15', 'Descripción de la incidencia 15', NOW(), 4, 'A');
+
+
 DELIMITER //
 
 CREATE PROCEDURE dashboard_info()
